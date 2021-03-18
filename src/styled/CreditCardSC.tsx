6ca0e4fa25ef9ff0f88592e56d17a2da80466880
sam <tweaksdoubt@mail.ru>
2021-03-18 18:29:37 +0300

    fix css

--- src/styled/CreditCardSC.tsx
+++ src/styled/CreditCardSC.tsx
@@ -5,11 +5,14 @@ interface PropTypes {
 }
 
 const animateBgVisa = keyframes`
-  from {
+  0% {
     background-position: 0 50%;
   }
-  to {
+  50% {
     background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
   }
 `;
 
